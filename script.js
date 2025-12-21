bfaf65d4299085ddb4c45d9d7e2203d3304dbc33
injamam64 <mdinjamam64@gmail.com>
2025-12-21 15:48:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// Student data stored directly in code
+const students = {
+    "rahul das": "101",
+    "suman roy": "12",
+    "amit saha": "103",
+    "priya sen": "104",
+    "md injamam": "105"
+};
+
+function findRoll() {
+    let name = document.getElementById("studentName").value.toLowerCase().trim();
+    let result = document.getElementById("result");
+
+    if (students[name]) {
+        result.innerHTML = "Your Roll Number is: " + students[name];
+        result.style.color = "green";
+    } else {
+        result.innerHTML = "Name not found!";
+        result.style.color = "red";
+    }
+}
